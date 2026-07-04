0446e70965dc1893aa193f99f90454fd633b521e
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-04 16:02:50 +0200

    feat: Add custom exceptions for user-related errors

---
+++ backend/src/FitConnect.Domain/Exceptions/EmailAlreadyRegisteredException.cs
@@ -0,0 +1,9 @@
+﻿namespace FitConnect.Domain.Exceptions;
+
+public class EmailAlreadyRegisteredException : Exception
+{
+    public EmailAlreadyRegisteredException(string email)
+        : base($"Email '{email}' is already registered.")
+    {
+    }
+}
