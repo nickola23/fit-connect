7fea6f4870dddef79fc1531ee677e6bcdb5deed2
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-04 16:08:27 +0200

    feat: Add PagedResponse<T> class for paginated API responses

---
+++ backend/src/FitConnect.Api/Contracts/Common/PagedResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace FitConnect.Api.Contracts.Common;
+
+public class PagedResponse<T>
+{
+    public required IReadOnlyList<T> Items { get; init; }
+    public required int TotalCount { get; init; }
+    public required int Page { get; init; }
+    public required int PageSize { get; init; }
+}
