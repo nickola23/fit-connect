a1df8e8e38ac4458b0dd71a990b8a6c58a75cdf0
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-04 16:05:13 +0200

    feat: Add IPasswordHasher interface and PagedResult<T> class

---
+++ backend/src/FitConnect.Application/Common/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace FitConnect.Application.Common;
+
+public interface IPasswordHasher
+{
+    string Hash(string plainTextPassword);
+    bool Verify(string plainTextPassword, string passwordHash);
+}
