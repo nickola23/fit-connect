043c588a3cb472b282f84a2158c4ffe317990ae5
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-05 22:54:36 +0200

    feat: Add interfaces for user access and token generation

---
+++ backend/src/FitConnect.Application/Common/ICurrentUserAccessor.cs
@@ -0,0 +1,10 @@
+﻿using FitConnect.Domain.Enums;
+
+namespace FitConnect.Application.Common;
+
+public interface ICurrentUserAccessor
+{
+    Guid? UserId { get; }
+    UserRole? Role { get; }
+    bool IsAuthenticated { get; }
+}
