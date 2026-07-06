4db465512c1b04aa7b2280648e29ba69d174f0fa
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-06 19:04:07 +0200

    feat: Add PricingTier model to represent trainer pricing options

---
+++ backend/src/FitConnect.Domain/Cooperations/PricingTier.cs
@@ -0,0 +1,19 @@
+﻿namespace FitConnect.Domain.Cooperations;
+
+public class PricingTier
+{
+    public Guid Id { get; }
+    public Guid TrainerId { get; }
+    public int SessionsPerWeek { get; }
+    public decimal MonthlyPrice { get; }
+    public bool Active { get; }
+
+    public PricingTier(Guid id, Guid trainerId, int sessionsPerWeek, decimal monthlyPrice, bool active)
+    {
+        Id = id;
+        TrainerId = trainerId;
+        SessionsPerWeek = sessionsPerWeek;
+        MonthlyPrice = monthlyPrice;
+        Active = active;
+    }
+}
