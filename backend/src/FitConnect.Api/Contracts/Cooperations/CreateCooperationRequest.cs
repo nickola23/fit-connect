e0ad3df77679876bf9f1ddee18a71577fd9f9baa
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-06 19:46:38 +0200

    feat: Add request and response contracts for Cooperation endpoints

---
+++ backend/src/FitConnect.Api/Contracts/Cooperations/CreateCooperationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FitConnect.Api.Contracts.Cooperations;
+
+public class CreateCooperationRequest
+{
+    public required Guid TrainerId { get; init; }
+    public Guid? PricingTierId { get; init; }
+    public bool IsFreeTrial { get; init; }
+}
