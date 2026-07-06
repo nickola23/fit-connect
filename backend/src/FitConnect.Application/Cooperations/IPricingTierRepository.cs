26c53caf8a49e80843212a6a514ac7f07f89e905
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-06 19:45:25 +0200

    feat: Add repository interfaces for Cooperation and PricingTier

---
+++ backend/src/FitConnect.Application/Cooperations/IPricingTierRepository.cs
@@ -0,0 +1,8 @@
+﻿using FitConnect.Domain.Cooperations;
+
+namespace FitConnect.Application.Cooperations;
+
+public interface IPricingTierRepository
+{
+    Task<PricingTier?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
