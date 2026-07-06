b7799e2bf9de3b7c805c598a8abdf45c130a12bd
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-06 19:45:10 +0200

    feat: Add RequestCooperationCommand and CooperationParticipants to handle cooperation requests

---
+++ backend/src/FitConnect.Application/Cooperations/RequestCooperationCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace FitConnect.Application.Cooperations;
+
+public class RequestCooperationCommand
+{
+    public required Guid TrainerId { get; init; }
+    public required Guid ClientId { get; init; }
+    public Guid? PricingTierId { get; init; }
+    public bool IsFreeTrial { get; init; }
+}
