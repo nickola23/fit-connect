fa203989d5e505cb7cb0a706820166612eb34177
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-07 22:10:02 +0200

    feat: Add request and response models for Exercise APIs

---
+++ backend/src/FitConnect.Api/Contracts/Exercises/UpdateExerciseRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FitConnect.Api.Contracts.Exercises;
+
+public class UpdateExerciseRequest
+{
+    public required string Name { get; init; }
+    public required int DefaultReps { get; init; }
+    public required int DefaultSets { get; init; }
+}
