6ef33e77c2cb190087cb923c0a31be7f9a5d4fd0
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-08 23:20:05 +0200

    feat: Add Equipment domain model

---
+++ backend/src/FitConnect.Domain/Equipment/Equipment.cs
@@ -0,0 +1,18 @@
+﻿namespace FitConnect.Domain.Equipment;
+
+public class Equipment
+{
+    public Guid Id { get; }
+    public string Name { get; private set; }
+
+    public Equipment(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public void Rename(string name)
+    {
+        Name = name;
+    }
+}
