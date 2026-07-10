30839a99cc5078ac5b297e95bad49500a53c5fb3
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-10 15:01:55 +0200

    feat: Add trainer review request/response contracts and average rating to TrainerResponse

---
+++ backend/src/FitConnect.Api/Contracts/TrainerReviews/TrainerReviewResponse.cs
@@ -0,0 +1,23 @@
+﻿using FitConnect.Domain.Reviews;
+
+namespace FitConnect.Api.Contracts.TrainerReviews;
+
+public class TrainerReviewResponse
+{
+    public required Guid Id { get; init; }
+    public required Guid TrainerId { get; init; }
+    public required Guid ClientId { get; init; }
+    public required int Rating { get; init; }
+    public string? Comment { get; init; }
+    public required DateOnly ReviewDate { get; init; }
+
+    public static TrainerReviewResponse FromDomain(TrainerReview review) => new()
+    {
+        Id = review.Id,
+        TrainerId = review.TrainerId,
+        ClientId = review.ClientId,
+        Rating = review.Rating,
+        Comment = review.Comment,
+        ReviewDate = review.ReviewDate
+    };
+}
