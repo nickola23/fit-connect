8816ac472102207ca2254d865ef632461efd60fd
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-10 14:37:24 +0200

    feat: Add trainer review entity and custom exception for unauthorized reviews

---
+++ backend/src/FitConnect.Domain/Exceptions/Reviews/ClientHasNotCooperatedWithTrainerException.cs
@@ -0,0 +1,9 @@
+﻿namespace FitConnect.Domain.Exceptions.Reviews;
+
+public class ClientHasNotCooperatedWithTrainerException : Exception
+{
+    public ClientHasNotCooperatedWithTrainerException(Guid clientId, Guid trainerId)
+        : base($"Client '{clientId}' must have had a cooperation with trainer '{trainerId}' before leaving a review.")
+    {
+    }
+}
