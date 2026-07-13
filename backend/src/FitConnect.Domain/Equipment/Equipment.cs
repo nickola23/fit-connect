21a8fe7c9eced8b9b010adc8588dad0b4c23af84
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-13 15:36:06 +0200

    feat: Add `EquipmentType` enum and update EquipmentService to support equipment type handling

--- backend/src/FitConnect.Domain/Equipment/Equipment.cs
+++ backend/src/FitConnect.Domain/Equipment/Equipment.cs
@@ -1,18 +1,23 @@
-﻿namespace FitConnect.Domain.Equipment;
+﻿using FitConnect.Domain.Enums;
+
+namespace FitConnect.Domain.Equipment;
 
 public class Equipment
 {
     public Guid Id { get; }
     public string Name { get; private set; }
+    public EquipmentType Type { get; private set; }
 
-    public Equipment(Guid id, string name)
+    public Equipment(Guid id, string name, EquipmentType type)
     {
         Id = id;
         Name = name;
+        Type = type;
     }
 
-    public void Rename(string name)
+    public void UpdateDetails(string name, EquipmentType type)
     {
         Name = name;
+        Type = type;
     }
 }
