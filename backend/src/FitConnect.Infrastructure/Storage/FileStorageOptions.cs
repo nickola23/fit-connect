6955343f25eadf2217d25ef5dfea9cff3cb2a676
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-16 14:44:35 +0200

    feat: Implement `LocalFileStorage` with file saving support and configurable options

---
+++ backend/src/FitConnect.Infrastructure/Storage/FileStorageOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace FitConnect.Infrastructure.Storage;
+
+public class FileStorageOptions
+{
+    public required string RootPath { get; init; }
+    public required string PublicPathPrefix { get; init; }
+}
