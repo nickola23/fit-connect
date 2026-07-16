2b39a56e30bc40d2175f505178a6623aac855f0f
nickola23 <nikolamilanovic2305@gmail.com>
2026-07-16 14:40:55 +0200

    feat: Add file categorization enums and custom exceptions for file validation

---
+++ backend/src/FitConnect.Domain/Exceptions/Files/UnsupportedFileTypeException.cs
@@ -0,0 +1,9 @@
+﻿namespace FitConnect.Domain.Exceptions.Files;
+
+public class UnsupportedFileTypeException : Exception
+{
+    public UnsupportedFileTypeException(string extension, IEnumerable<string> allowedExtensions)
+        : base($"File type '{extension}' is not supported. Allowed types: {string.Join(", ", allowedExtensions)}.")
+    {
+    }
+}
